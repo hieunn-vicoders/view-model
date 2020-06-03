d72d3343a75ffd115f92fec2dfe692958d94e798
sonhaichu <sonhaichu@gmail.com>
2020-06-03 15:20:38 +0700

    init

---
+++ src/app/Providers/ViewModelServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace VCComponent\Laravel\ViewModel\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ViewModelServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any package services
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register any package services
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
